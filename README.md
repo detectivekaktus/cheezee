5e470891eedb2a45b61227ff096fc0b82b94b7d1
Artiom Astashonak <artiomastashonak@gmail.com>
2024-07-06 23:26:47 +0200

    2 moves are logged on the same line.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Cheezee (pronounced as cheese) is an ncurses chess client written for Linux. Pla
 
 The client supports all legal moves in chess (including en-passant, casteling) and can understand whether the position is in checkmate or in stalemate.
 
+All the moves played are logged to the right hand side of the board with the standard notation except that the black pieces are annotated with lowercase letters.
+
 ## Compilation
 In order to compile the program, you need to have a C compiler (either GCC or clang) and the ncurses library installed on your system.
 
--- src/logger.c
+++ src/logger.c
@@ -15,6 +15,14 @@ void log_move(Logger *logger, int srow, int scol, int erow, int ecol) {
   MoveString *str = translate_move(logger, srow, scol, erow, ecol);
   if (logger->move == 1) {
     mvwaddstr(logger->log_win->win, 2, 10, str->content);
+    wrefresh(logger->log_win->win);
+    DESTROY_STRING(str);
+    return;
+  }
+  if (logger->move % 2 != 0) {
+    mvwaddstr(logger->log_win->win, getcury(logger->log_win->win) + 2, 10, str->content);
+  } else if (logger->move % 2 == 0) {
+    mvwaddstr(logger->log_win->win, getcury(logger->log_win->win), getcurx(logger->log_win->win) + 5, str->content);
   } else if (logger->log_win->y - getcury(logger->log_win->win) <= 10) {
     wclear(logger->log_win->win);
     box(logger->log_win->win, 0, 0);
@@ -77,7 +85,11 @@ MoveString *translate_move(Logger *logger, int srow, int scol, int erow, int eco
       }
     }
   }
-  if (!is_empty(logger->board->current[erow][ecol])) STRING_APPEND(str, 'x');
+  if (!is_empty(logger->board->current[erow][ecol]) &&
+    (logger->board->current[srow][scol] == PAWN || logger->board->current[srow][scol] == PAWN + BLACK)) {
+    STRING_APPEND(str, 'a' + scol);
+    STRING_APPEND(str, 'x');
+  } else if (!is_empty(logger->board->current[erow][ecol])) STRING_APPEND(str, 'x');
   STRING_APPEND(str, 'a' + ecol);
   STRING_APPEND(str, '8' - erow);
 
